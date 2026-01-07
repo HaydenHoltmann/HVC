f5497acda80384751d060e42d8a6f722914e6c98
Hayden <haydenholtmann3@gmail.com>
2026-01-07 15:20:54 +0200

    Hashing and Compression function added

--- main.py
+++ main.py
@@ -2,4 +2,7 @@ import vers_ctrl
 
 h1 = vers_ctrl.HVC()
 
-h1.init()
+# h1.hash_object("blob", "what is up, doc?")
+
+# h1.add(["main.cpp", "class.h", "class.cpp"])
+h1.add(".")
--- vers_ctrl.py
+++ vers_ctrl.py
@@ -1,21 +1,71 @@
 import shutil
 import os
+import hashlib
+import zlib
+from pathlib import Path
 
 
 class HVC:
     # Initializes a directory as a repository
+    def __init__(self):
+        self.template_directory = "Template/"
+        self.cwd = os.path.dirname(os.path.abspath(__file__))
+        self.ignore_content = self.process_ignore()
+
     def init(self):
         # TODO: Change template directory to config install/wherever install files go directory
         # Install script/Installation method must create the Template folder
-        template_directory = "Template/"
         file_path = os.path.abspath(__file__)
-        repository_directory = os.path.dirname(file_path) + "/.hvc"
+        repository_directory = self.cwd + "/.hvc"
 
         if not os.path.exists(repository_directory):
-            shutil.copytree(template_directory, repository_directory)
+            shutil.copytree(self.template_directory, repository_directory)
             print(f"Initialized new HVC repository in {repository_directory}")
         else:
             print("Directory already exists.")
 
-    def hash_object(self):
-        return 0
+    def hash_object(self, obj_type, content):
+        header = f"{obj_type} {len(content.encode('utf-8'))}\0"
+
+        to_hash = header + content
+
+        # Hashing Stuff
+        obj_hash = hashlib.sha1(to_hash.encode("utf-8"))
+
+        sha1 = obj_hash.hexdigest()
+
+        print(sha1)
+
+        # Compression Stuff
+        zlib_content = zlib.compress(to_hash.encode("utf-8"))
+
+        # File Stuff
+        path = Path(".hvc/objects/" + sha1[0:2] + "/" + sha1[2:])
+
+        path.parent.mkdir(exist_ok=True, parents=True)
+
+        path.write_bytes(zlib_content)
+
+    def add(self, files):
+        if files[0] == ".":
+            print(os.listdir(self.cwd))
+        else:
+            print(self.cwd)
+
+    def update_index(self):
+        hello = "hello"
+
+    def process_ignore(self):
+        ignore_file = open(".hvc_ignore")
+        ignore_output = []
+
+        # TODO: Add support for things like file types etc. that need to be in an ignore file. You might have to separate into different lists
+        for line in ignore_file:
+            if line[0] == "#" or line == " " or line == "\n":
+                pass
+            else:
+                ignore_output.append(line[: len(line) - 1])
+
+        ignore_file.close()
+
+        return ignore_output
