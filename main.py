3af3c115b69fa18ecfc8c757448149006f5b89ee
Hayden <haydenholtmann3@gmail.com>
2026-01-09 08:37:11 +0200

    Added cat function that returns object content

--- main.py
+++ main.py
@@ -5,4 +5,6 @@ h1 = vers_ctrl.HVC()
 # h1.hash_object("blob", "what is up, doc?")
 
 # h1.add(["main.cpp", "class.h", "class.cpp"])
-h1.add(".")
+# h1.add(["main.py", "vers_ctrl", "fake_file.py"])
+h1.hash_object("blob", "Some other blob")
+print(h1.cat_content("bd9dbf5aae1a3862dd1526723246b20206e5fc37"))
--- vers_ctrl.py
+++ vers_ctrl.py
@@ -11,26 +11,25 @@ class HVC:
         self.template_directory = "Template/"
         self.cwd = os.path.dirname(os.path.abspath(__file__))
         self.ignore_content = self.process_ignore()
+        self.repository_directory = self.cwd + "/.hvc"
+        self.objects_directory = self.repository_directory + "/objects"
 
-        print(self.ignore_content[0])
-        print(self.ignore_content[1])
+        directories_ignored = [
+            x for x in os.listdir(self.cwd) if x not in self.ignore_content[0]
+        ]
 
         self.directory_files = [
-            x
-            for x in os.listdir(self.cwd)
-            if x not in self.ignore_content[0] not in self.ignore_content[1]
+            x for x in directories_ignored if x not in self.ignore_content[1]
         ]
-        # os.listdir(self.cwd)
 
     def init(self):
         # TODO: Change template directory to config install/wherever install files go directory
         # Install script/Installation method must create the Template folder
-        file_path = os.path.abspath(__file__)
-        repository_directory = self.cwd + "/.hvc"
+        # file_path = os.path.abspath(__file__)
 
-        if not os.path.exists(repository_directory):
-            shutil.copytree(self.template_directory, repository_directory)
-            print(f"Initialized new HVC repository in {repository_directory}")
+        if not os.path.exists(self.repository_directory):
+            shutil.copytree(self.template_directory, self.repository_directory)
+            print(f"Initialized new HVC repository in {self.repository_directory}")
         else:
             print("Directory already exists.")
 
@@ -50,7 +49,12 @@ class HVC:
         zlib_content = zlib.compress(to_hash.encode("utf-8"))
 
         # File Stuff
-        path = Path(".hvc/objects/" + sha1[0:2] + "/" + sha1[2:])
+        if obj_type == "index":
+            path = Path(".hvc/index")
+            print("Index was run")
+        else:
+            # path = Path(".hvc/objects/" + sha1[0:2] + "/" + sha1[2:])
+            path = Path(f"{self.objects_directory}/" + sha1[0:2] + "/" + sha1[2:])
 
         path.parent.mkdir(exist_ok=True, parents=True)
 
@@ -58,12 +62,21 @@ class HVC:
 
     def add(self, files):
         if files[0] == ".":
-            print(self.directory_files)
+            # Adds all files/directories in the repository to the index
+            self.update_index(self.directory_files)
         else:
-            print(self.cwd)
+            valid_files = []
+            # Checks for valid file names
+            for i in range(len(files)):
+                if files[i] in self.directory_files:
+                    valid_files.append(files[i])
+                else:
+                    print(f"{files[i]} not a part of this repository")
 
-    def update_index(self):
-        hello = "hello"
+            self.update_index(files)
+
+    def update_index(self, files):
+        self.hash_object("index", "Different stuff")
 
     def process_ignore(self):
         ignore_file = open(".hvc_ignore")
@@ -86,3 +99,22 @@ class HVC:
         ignore_file.close()
 
         return ignore_output
+
+    # Returns the content of a compressed object file
+    def cat_content(self, hash):
+        hash_folder = hash[:2]
+        hash_file = hash[2:]
+
+        hashed_file = open(f"{self.objects_directory}/{hash_folder}/{hash_file}", "rb")
+        toUnhash = hashed_file.read()
+
+        unhashed_content = zlib.decompress(toUnhash)
+        unhashed_split = unhashed_content.split(b"\0")
+
+        content_output = unhashed_split[1].decode("utf-8")
+
+        return content_output
+
+    # Returns the type of an object file
+    def cat_type(self, hash):
+        pass
